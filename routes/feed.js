f72e953b796fbd15fb1e6d676e3b4895cb7fe99d
mpaternostro <marcelodpaternostro@gmail.com>
2021-05-02 20:25:26 -0300

    feat: add isAuth middleware

--- routes/feed.js
+++ routes/feed.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
 
 const { getPosts, getPost, createPost, updatePost, deletePost } = require("../controllers/feed");
+const isAuth = require("../middlewares/is-auth");
 const createPostValidation = require("../validators/createPost");
 const updatePostValidation = require("../validators/updatePost");
 
-router.get("/posts", getPosts);
+router.get("/posts", isAuth, getPosts);
 
-router.get("/post/:postId", getPost);
+router.get("/post/:postId", isAuth, getPost);
 
-router.post("/post", createPostValidation, createPost);
+router.post("/post", isAuth, createPostValidation, createPost);
 
-router.put("/post/:postId", updatePostValidation, updatePost);
+router.put("/post/:postId", isAuth, updatePostValidation, updatePost);
 
-router.delete("/post/:postId", deletePost);
+router.delete("/post/:postId", isAuth, deletePost);
 
 module.exports = router;
