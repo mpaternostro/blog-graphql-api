3b49c567d345739d52416874441f7f418779f1d0
mpaternostro <marcelodpaternostro@gmail.com>
2021-05-29 20:29:02 -0300

    fix: clear unused files

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
-  next();
+  return next();
 });
 
 app.use(auth);
--- validators/updateStatus.js
+++
@@ -1,3 +0,0 @@
-const { body } = require("express-validator");
-
-module.exports = [body("status").notEmpty().withMessage("Please enter new status.")];
